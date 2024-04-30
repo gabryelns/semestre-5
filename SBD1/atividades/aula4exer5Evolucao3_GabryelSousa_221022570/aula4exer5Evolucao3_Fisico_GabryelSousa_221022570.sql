cd0fd50758cb40c87c15f3e96be1391e1db34ce2
gabryelns <gabryelnick91@gmail.com>
2024-04-30 14:32:29 -0300

    add atividade mal feita de bd1

---
+++ SBD1/atividades/aula4exer5Evolucao3_GabryelSousa_221022570/aula4exer5Evolucao3_Fisico_GabryelSousa_221022570.sql
@@ -0,0 +1,30 @@
+﻿-- -------- < AULA 4 EXER 5 EVOLUCAO 3 > --------
+--
+--                    SCRIPT DE CRIACAO (DDL)
+--
+-- Data Criacao ...........: 29/04/2024
+-- Autor(es) ..............: Gabryel Nicolas Soares de Sousa
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: bdFinanceiro
+--
+-- PROJETO => 01 Base de Dados
+--         => 1 Tabela
+-- 
+-- Ultimas Alteracoes
+--   29/04/2024 => criacao do primeiro projeto.
+--            
+--
+-- ---------------------------------------------------------
+create table database cadastrar
+default character set utf8
+default collate utf8_general_ci;
+
+create table 'paciente' (
+  'idPaciente' id not null auto_increment,
+  'nomeCompleto' varchar(30) not null,
+  'dtNascimento' date,
+  'sexo' varchar(10),
+  'telefone' int,
+  'endereco',
+  primary key (id) 
+) default charset = utf8;
