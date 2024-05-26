1de1ddb12c80a284398b7c938bbb941a5ff1bb55
gabryelns <gabryelnick91@gmail.com>
2024-05-26 02:39:22 -0300

    Soma 2

---
+++ Compiladores/lista 3/soma2.pas
@@ -0,0 +1,13 @@
+program soma2(input, output);
+
+var
+    num1: Longint;
+    num2: Longint;
+    result: longint;
+
+begin
+    readln(num1);
+    readln(num2);
+    result := num1 + num2;
+    writeln(result);
+end.
